4126b53e2c1a7b4e25bc6d7d63ef5ba9202b7e1c
gamlethot <shamrai.nikita@gmail.com>
2022-05-10 16:17:11 +0300

    Test commit on new branch

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import Service.FibbonachiGenerator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FibbonachiGenerator fibbonachiGenerator = new FibbonachiGenerator();
+
+                       //TYPE YOUR Fibonacci Index HERE ↓
+        fibbonachiGenerator.createFibboList(10);
+    }
+}
